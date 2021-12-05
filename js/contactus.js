89a5c5558a07a6665c0ead19a7ff3db5ca2044e7
Mark VanDam <mark.vandam@flemingcollege.ca>
2021-12-05 14:22:23 -0500

    added comments

--- js/contactus.js
+++ js/contactus.js
@@ -1,12 +1,11 @@
 function contactRequest(form) {
+    //created by Mark VanDam
     var inName = form.name.value;
     var subject = "Web Form Submission"
-    //alert("Your Name Is: "+ inName);
     var inEmail = form.email.value;
-    //alert("Your Email Is: "+ inEmail);
     var inComm = form.comments.value;
-    var body = inName + "'s Comments:\n" + inComm;			
-    //alert("Your Name Is: "+ inComm);
+    var body = inName + "'s Comments:\n" + inComm;
+    //note: fictional email address, does not exist.			
     location = "mailto:info@navscapegeo.com?subject="+subject+"&cc="+inEmail+"&body="+body;
     return true;
 }
